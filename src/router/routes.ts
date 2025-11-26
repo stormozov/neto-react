079c7a2b050235759622af08c7c81cb5a9bba885
stormozov <web@stormozov.ru>
2025-11-26 22:28:12 +0300

    fix(config): испр. base url для деплоя на gh-pages

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,28 +2,33 @@ import { HomePage, NotFoundPage, ProjectPage } from '@pages';
 import { createBrowserRouter } from 'react-router';
 import App from '@/App';
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    Component: App,
-    children: [
-      {
-        index: true,
-        Component: HomePage,
-      },
-      {
-        path: 'projects',
-        children: [
-          {
-            path: ':projectId',
-            Component: ProjectPage,
-          },
-        ],
-      },  
-      {
-        path: '*',
-        Component: NotFoundPage,
-      },    
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+	[
+		{
+			path: "/",
+			Component: App,
+			children: [
+				{
+					index: true,
+					Component: HomePage,
+				},
+				{
+					path: "projects",
+					children: [
+						{
+							path: ":projectId",
+							Component: ProjectPage,
+						},
+					],
+				},
+				{
+					path: "*",
+					Component: NotFoundPage,
+				},
+			],
+		},
+	],
+	{
+		basename: import.meta.env.BASE_URL,
+	},
+);
